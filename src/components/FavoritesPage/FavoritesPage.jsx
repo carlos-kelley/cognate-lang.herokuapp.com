80c218d5becb63a7eaa2073b07583d8a875be3ea
Carlos Kelley <carloskelley@protonmail.com>
2022-07-22 00:51:59 -0500

    Favorite search works

--- src/components/FavoritesPage/FavoritesPage.jsx
+++ src/components/FavoritesPage/FavoritesPage.jsx
@@ -27,7 +27,10 @@ function FavoritesPage() {
   const [search, setSearch] = useState("");
 
   const checkLength = () => {
-    console.log ("favoritesSearch reducer is", favoritesSearch);
+    console.log(
+      "favoritesSearch reducer is",
+      favoritesSearch
+    );
     if (search.length === 0) {
       console.log("no search");
     } else {
@@ -41,34 +44,33 @@ function FavoritesPage() {
     dispatch({ type: "FETCH_FAVORITES" });
     console.log(event.currentTarget);
     console.log("favs", favorites);
-    console.log ("favs search", favoritesSearch);
+    console.log("favs search", favoritesSearch);
   }, []);
   return (
     <div className="container">
-      {favorites.length === 0 ? (
-        <p>loading...</p>
-      ) : (
-        <div>
-          <div className="favoritesDiv">
-            <h1>Favorites Page</h1>
-            {/* <FavoritesSearch /> */}
-          </div>
-            <button onClick={checkLength}></button>
-            {JSON.stringify({ favoritesSearch })};
-          <input
-            type="text"
-            placeholder="Search favorites"
-            onChange={(event) => {
-              dispatch({
-                type: "FETCH_FAVORITES_SEARCH",
-                payload: event.target.value,
-              });
-              setSearch(event.target.value);
-            }}
-          />
-
+      <div>
+        <div className="favoritesDiv">
+          <h1>Favorites Page</h1>
+          {/* <FavoritesSearch /> */}
+        </div>
+        <button onClick={checkLength}></button>
+        {JSON.stringify({ favoritesSearch })};
+        <input
+          type="text"
+          placeholder="Search favorites"
+          onChange={(event) => {
+            dispatch({
+              type: "FETCH_FAVORITES_SEARCH",
+              payload: event.target.value,
+            });
+            setSearch(event.target.value);
+          }}
+        />
+        {/* only do this is search is equal to 0 */}
+        {search === "" ? (
           <section className="favoritesSection">
             {/* loop through our favorites and display them */}
+
             {favorites.map((favorite) => {
               return (
                 <div
@@ -101,8 +103,47 @@ function FavoritesPage() {
               );
             })}
           </section>
-        </div>
-      )}
+        ) : (
+          <section className="favoritesSearchSection">
+            {/* loop through our favoritesSearch and display them */}
+
+            {favoritesSearch.map(
+              (favoriteSearch) => {
+                return (
+                  <div
+                    // duplicate keys
+
+                    key={favoriteSearch.id}
+                  >
+                    {/* display the favoriteSearch in english */}
+                    {/* help */}
+                    <h3 onClick={navToWord}>
+                      {favoriteSearch.english},
+                      {favoriteSearch.french},
+                      {favoriteSearch.italian},
+                      {favoriteSearch.spanish},
+                      {favoriteSearch.portuguese}
+                    </h3>
+                    {/* render a delete button */}
+                    <button
+                      className="btn btn_sizeSm"
+                      onClick={() => {
+                        dispatch({
+                          type: "DELETE_FAVORITE",
+                          payload:
+                            favoriteSearch.id, //i think this is wrong maybet
+                        });
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                );
+              }
+            )}
+          </section>
+        )}
+      </div>
     </div>
   );
 }
