162bf6667c33d9c28004b602658fc7536f427f82
Carlos Kelley <carloskelley@protonmail.com>
2022-07-22 01:15:08 -0500

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Footer.css";
+import LogOutButton from '../LogOutButton/LogOutButton';
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
@@ -8,7 +9,12 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <footer> Carlos Kelley &copy; 2022. All Rights Reserved. </footer>
+    <div>
+    <footer>
+      <p>Carlos Kelley &copy; 2022. All Rights Reserved. <LogOutButton className="navLink" /></p>
+    </footer>
+    
+    </div>
   );
 }
 
