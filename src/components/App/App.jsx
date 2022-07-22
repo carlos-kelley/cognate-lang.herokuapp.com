50f32c365903c9add9f06c151d44b378c32a3fc7
Carlos Kelley <carloskelley@protonmail.com>
2022-07-21 22:24:41 -0500

    delete route works with a static payload

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -69,7 +69,7 @@ function App() {
           <ProtectedRoute
             // logged in shows UserPage else shows LoginPage
             exact
-            path="/word"
+            path="/word/:id"
           >
             <WordPage />
           </ProtectedRoute>
--- src/redux/sagas/favorites.saga.js
+++ src/redux/sagas/favorites.saga.js
@@ -24,7 +24,7 @@ function* fetchFavorites() {
 // worker Saga: will be fired on "DELETE_FAVORITE" actions
 function* deleteFavorite(action) {
   console.log("deleting fav");
-  let deleteWord = 300; //THIS IS WRONG. It should be action.payload, but the IDs are incorrect in the database so it won't work
+  let deleteWord = 4; //THIS IS WRONG. It should be action.payload, but the IDs are incorrect in the database so it won't work
   try {
     console.log("delete action.payload:", deleteWord);
     yield axios.delete(
