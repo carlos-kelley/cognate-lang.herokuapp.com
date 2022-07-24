4adb50d3c9c870cfa5059bbba25811063ecebc49
Carlos Kelley <carloskelley@protonmail.com>
2022-07-24 06:07:07 -0500

    conditional rendering done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Footer.css";
 import LogOutButton from "../LogOutButton/LogOutButton";
-import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
@@ -9,26 +9,22 @@ import { useLocation } from "react-router-dom";
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  const location = useLocation();
+  const user = useSelector((store) => store.user);
+
   return (
     <div>
       <footer>
-        {location.pathname === "/login" ? (
+        {!user.id && (
           <p>
             Carlos Kelley &copy; 2022. All Rights
             Reserved.
           </p>
-        ) : location.pathname ===
-          "/registration" ? (
+        )}
+        {user.id &&(
           <p>
-            Carlos Kelley &copy; 2022. All Rights
-            Reserved.
-          </p>
-        ) : (
-          <p>
-            Carlos Kelley &copy; 2022. All Rights
-            Reserved.
-            <LogOutButton className="navLink" />
+          Carlos Kelley &copy; 2022. All Rights
+          Reserved.
+          <LogOutButton className="navLink" />
           </p>
         )}
       </footer>
