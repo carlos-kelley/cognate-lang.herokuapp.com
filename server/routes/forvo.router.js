d31a1875dd03cbb0d7eb3e4cd0860a22d6ab6865
Carlos Kelley <carloskelley@protonmail.com>
2022-07-26 21:25:29 -0500

    commented out unused forvo server. is that set up right?

--- server/routes/forvo.router.js
+++ server/routes/forvo.router.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const router = express.Router();
-const axios = require("axios");
-require("dotenv").config();
+// const express = require("express");
+// const router = express.Router();
+// const axios = require("axios");
+// require("dotenv").config();
 
-console.log ("process.env.REACT_APP_FORVOKEY in forvo router : ", process.env.REACT_APP_FORVOKEY);
+// console.log ("process.env.REACT_APP_FORVOKEY in forvo router : ", process.env.REACT_APP_FORVOKEY);
 
-axios
-  .get(
-    `https://apifree.forvo.com/key/${process.env.REACT_APP_FORVOKEY}/format/json/action/word-pronunciations/word/cat/language/en`
-  )
-  .then((resp) => {
-    console.log("forvo resp: ", resp.data);
-  });
+// axios
+//   .get(
+//     `https://apifree.forvo.com/key/${process.env.REACT_APP_FORVOKEY}/format/json/action/word-pronunciations/word/cat/language/en`
+//   )
+//   .then((resp) => {
+//     console.log("forvo resp: ", resp.data);
+//   });
 
 
-module.exports = router;
+// module.exports = router;
