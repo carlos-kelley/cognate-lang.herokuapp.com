f7acc6c10ca1390f3435f28d20e1fcc1e45a4e5c
Carlos Kelley <carloskelley@protonmail.com>
2023-03-03 11:14:39 -0600

    french audio working

--- src/components/WordPage/WordPage.jsx
+++ src/components/WordPage/WordPage.jsx
@@ -9,7 +9,6 @@ import {
 } from "react-redux";
 import { useRef } from "react";
 import { useHistory } from "react-router-dom";
-import { Howl, Howler } from "howler";
 import GlobalSearchPage from "../GlobalSearchPage/GlobalSearchPage";
 import { useParams } from "react-router-dom";
 import "./WordPage.css";
@@ -40,11 +39,6 @@ function WordPage() {
   const thisWord = useSelector(
     (store) => store.thisWord
   );
-  const isMountedEnglish = useRef(false);
-  const isMountedFrench = useRef(false);
-  const isMountedItalian = useRef(false);
-  const isMountedSpanish = useRef(false);
-  const isMountedPortuguese = useRef(false);
 
   //to use the id and word from the url
   const params = useParams();
@@ -79,87 +73,68 @@ function WordPage() {
   const forvoPortuguese = useSelector(
     (store) => store.forvoPortuguese
   );
+  const [selectedWord, setSelectedWord] =
+    useState(null);
+
+  const audioRef = useRef(null);
+
+  const forvoFrenchRef = useRef(null);
 
   useEffect(() => {
-    if (isMountedEnglish.current) {
-      console.log(
-        "in starteng, forvo path is: ",
-        forvoEnglish
-      );
-      const audio = new Howl({
-        src: [forvoEnglish.items[0].pathmp3],
-        autoplay: true,
-      });
-      audio.play();
-    } else {
-      isMountedEnglish.current = true;
-    }
-  }, [forvoEnglish]);
-
-  useEffect(() => {
-    if (isMountedFrench.current) {
-      console.log(
-        "in startfr, forvo path is: ",
-        forvoFrench
-      );
-    } else {
-      isMountedFrench.current = true;
-    }
+    forvoFrenchRef.current = forvoFrench;
   }, [forvoFrench]);
 
   useEffect(() => {
-    if (isMountedSpanish.current) {
-      console.log(
-        "in startsp, forvo path is: ",
-        forvoSpanish
-      );
+    if (
+      selectedWord &&
+      forvoFrenchRef.current === forvoFrench &&
+      forvoFrench &&
+      forvoFrench.items &&
+      forvoFrench.items.length
+    ) {
       const audio = new Audio(
-        forvoSpanish.items[0].pathmp3
+        forvoFrench.items[0].pathmp3
       );
-      audio.muted = true;
-      audio.playsInline = true;
-      audio.play();
-    } else {
-      isMountedSpanish.current = true;
+      console.log(
+        "forvoFrench in func is: ",
+        forvoFrench.items[0].pathmp3
+      );
+      audioRef.current = audio;
+      Promise.resolve()
+        .then(() => {
+          audioRef.current.pause(); // pause the old audio before playing new audio
+        })
+        .then(() => {
+          audioRef.current.currentTime = 0; // reset the audio to start from the beginning
+          return new Promise((resolve) =>
+            setTimeout(resolve, 1000)
+          );
+        })
+        .then(() => {
+          audioRef.current.play(); // play the new audio
+        });
     }
-  }, [forvoSpanish]);
+  }, [forvoFrench, selectedWord]);
 
-  useEffect(() => {
-    if (isMountedItalian.current) {
-      console.log(
-        "in startit, forvo path is: ",
-        forvoItalian
-      );
-      const audio = new Audio(
-        forvoItalian.items[0].pathmp3
-      );
-      audio.muted = true;
-      audio.playsInline = true;
-      audio.play();
-    } else {
-      isMountedItalian.current = true;
-    }
-  }, [forvoItalian]);
+  function cleanFrenchWord(french) {
+    const regex =
+      /^(l'|le |la )|(, .*?)|(\(.*?\))|(les )/gi;
+    return french.replace(regex, "").trim();
+  }
 
-  useEffect(() => {
-    if (isMountedPortuguese.current) {
-      console.log(
-        "in startpo, forvo path is: ",
-        forvoPortuguese
-      );
-      const audio = new Audio(
-        forvoPortuguese.items[0].pathmp3
-      );
-      audio.muted = true;
-      audio.playsInline = true;
-      audio.play();
-    } else {
-      isMountedPortuguese.current = true;
-    }
-  }, [forvoPortuguese]);
+  // Define a new function to play the audio
+  function playForvoFrenchAudio(
+    french,
+    dispatch
+  ) {
+    const cleanFrench = cleanFrenchWord(french);
+    dispatch({
+      type: "FETCH_FORVO_FRENCH",
+      payload: cleanFrench,
+    });
+  }
 
   const [word, setWord] = useState(null);
-  // const [audioPath, setAudioPath] = useState(null);
   //if i pull all the words can i map them with a filter?
 
   useEffect(() => {
@@ -172,55 +147,11 @@ function WordPage() {
     });
   }, []);
 
-  // const handleRandom = () => {
-  //   console.log("in handleRandom: ");
-  //   setRandom(randomNumberInRange(3, 352));
-  //   console.log("random becomes: ", random);
-  //   //if random is not null or 0 push to the word page of the random number
-  //   if (random !== null && random !== 0) {
-  //     history.push(`/word/${random}`);
-  //   }
-  // };
-
-  const handleForvoFrench = (action) => {
-    //send word to saga to get forvo audio
-    dispatch({
-      type: "FETCH_FORVO_FRENCH",
-      payload: word.french,
-    });
-  };
-
-  const handleForvoSpanish = (action) => {
-    //send word to saga to get forvo audio
-    dispatch({
-      type: "FETCH_FORVO_SPANISH",
-      payload: word.spanish,
-    });
-  };
-
-  const handleForvoItalian = (action) => {
-    //send word to saga to get forvo audio
-    dispatch({
-      type: "FETCH_FORVO_ITALIAN",
-      payload: word.italian,
-    });
-  };
-
-  const handleForvoPortuguese = (action) => {
-    //send word to saga to get forvo audio
-    dispatch({
-      type: "FETCH_FORVO_PORTUGUESE",
-      payload: word.portuguese,
-    });
-  };
-
   return (
     <div className="containerWordPage">
       <div className="wordSearchContainer withToggle">
         <GlobalSearchPage />
       </div>
-
-      {/* map through thisWord array */}
 
       {thisWord.map((word) => {
         return (
@@ -232,7 +163,6 @@ function WordPage() {
               <h3
                 className="englishWord"
                 onClick={() => {
-                  //create regex to remove everything inside parentheses or after a comma
                   const regex =
                     /(, (.*)|\((.*?)\))/g;
                   const newEnglish =
@@ -249,83 +179,35 @@ function WordPage() {
                     "action.payload in forvo eng: ",
                     word.english
                   );
-                  // console.log ("forvo in func is: ", forvo);
-                  // const audio = new Audio(
-                  //   forvo.items[0].pathmp3
-                  // );
-                  // audio.play();
                 }}
               >
                 {word.english}
               </h3>
             )}
             {toggleFrench && (
-              <h3
-                className="frenchWord"
-                onClick={() => {
-                  //create regex to remove "l'"" and "la " and "le " from word.french
-                  const regex =
-                    /(^l'|^le |^la )/gi;
-                  const newFrench =
-                    word.french.replace(
-                      regex,
-                      ""
+              <>
+                <h3 className="frenchWord">
+                  {cleanFrenchWord(word.french)}
+                </h3>
+                <button
+                  className="playButton"
+                  onClick={() => {
+                    setSelectedWord(word);
+                    playForvoFrenchAudio(
+                      word.french,
+                      dispatch
                     );
-                  console.log(
-                    "new french is: ",
-                    newFrench
-                  );
-                  //use last regex and remove anything inside parentheses from newFrench
-                  const regex2 = /\((.*?)\)/gi;
-                  const newFrench2 =
-                    newFrench.replace(regex2, "");
-                  console.log(
-                    "newFrench2 is: ",
-                    newFrench2
-                  );
-                  //regex to remove anything after a comma from newFrench2
-                  const regex3 = /, (.*)/gi;
-                  const newFrench3 =
-                    newFrench2.replace(
-                      regex3,
-                      ""
-                    );
-                  console.log(
-                    "newFrench3 is: ",
-                    newFrench3
-                  );
-                  //regex to remove "les " from newFrench3
-                  const regex4 = /les /gi;
-                  const newFrench4 =
-                    newFrench3.replace(
-                      regex4,
-                      ""
-                    );
-                  console.log(
-                    "newFrench4 is: ",
-                    newFrench4
-                  );
-                  //send word to saga to get forvo audio
-                  dispatch({
-                    type: "FETCH_FORVO_FRENCH",
-                    payload: newFrench4,
-                  }).then(() => {
-                    const audio = new Audio(
-                      forvoFrench.items[0].pathmp3
-                    );
-                    audio.play();
-                  });
-                }}
-              >
-                {word.french}
-              </h3>
+                  }}
+                >
+                  Play
+                </button>
+              </>
             )}
-            {/* el la () los las  , */}
+
             {toggleSpanish && (
               <h3
                 className="spanishWord"
                 onClick={() => {
-                  //create regex to remove "la " and "el " and "los " and "las " and anything after a comma and anything in parentheses from word.spanish
                   const regex =
                     /(^la |^el |^los |^las |, (.*)|\((.*?)\))/gi;
                   const newSpanish =
@@ -351,7 +233,6 @@ function WordPage() {
               <h3
                 className="italianWord"
                 onClick={() => {
-                  //create regex to remove "il " and "lo " and "la " and "gli " and "l'" and "i " and "le " and anything after a comma and anything in parentheses from word.spanish
                   const regex =
                     /(^il |^lo |^la |^gli |^l'|^i |^le |, (.*)|\((.*?)\))/gi;
                   const newItalian =
@@ -377,7 +258,6 @@ function WordPage() {
               <h3
                 className="portugueseWord"
                 onClick={() => {
-                  //create regex to remove "o " "a " "os " "as " and anything after a comma and anything in parentheses from word.portuguese
                   const regex =
                     /(^o |^a |^os |^as |, (.*)|\((.*?)\))/gi;
                   const newPortuguese =
@@ -405,14 +285,6 @@ function WordPage() {
       <div className="addFavContainer">
         <AddFavoriteWordPage />
       </div>
-      {/* <div className="randomWordContainer">
-        <button
-          onClick={handleRandom}
-          className="randomSearch"
-        >
-          Random Word
-        </button>
-      </div> */}
     </div>
   );
 }
